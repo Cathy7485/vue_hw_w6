5f18d1cfff02561b7e2204cc66088b57b747b4e9
cathy <cathy141153@gmail.com>
2023-02-26 10:36:06 +0800

    update files

---
+++ .deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+# 部署到 https://cathy7485.github.io
+# git push -f git@github.com:cathy7485/cathy7485.github.io.git master
+
+# 部署到 https://cathy7485.github.io/vue_hw_w6
+git push -f git@github.com:cathy7485/vue_hw_w6.git master:gh-pages
+
+cd -

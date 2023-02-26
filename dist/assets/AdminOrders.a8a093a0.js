6585541299c58c790ed504d64c0424e666edbd82
cathy <cathy141153@gmail.com>
2023-02-26 12:40:11 +0800

    delete dist

--- dist/assets/AdminOrders.a8a093a0.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index.281c0acc.js";const e={};function c(n,t){return" \u5F8C\u53F0\u8A02\u55AE\u5217\u8868 "}const s=r(e,[["render",c]]);export{s as default};
--- dist/assets/AdminProducts.b0971545.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index.281c0acc.js";const c={};function t(e,n){return" \u5F8C\u53F0\u7522\u54C1\u5217\u8868 "}const o=r(c,[["render",t]]);export{o as default};
